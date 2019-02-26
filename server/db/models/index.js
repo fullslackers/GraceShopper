b3c799f81bcd883343e9075f1c15291629e8f27c
Rimma <rim23ma@gmail.com>
2019-02-26 10:39:10 -0600

    assosiations between models

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,18 +1,47 @@
 const User = require('./user')
+const Product = require('./product')
+const Order = require('./order')
+const Review = require('./review')
+const Category = require('./category')
+const db = require('../db')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+const ProductCategory = db.define(
+  'product-category',
+  {},
+  {
+    freezeTableName: true
+  }
+)
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+Product.belongsToMany(Category, {through: ProductCategory})
+Category.belongsToMany(Product, {through: ProductCategory})
+
+const OrderProduct = db.define(
+  'order-product',
+  {},
+  {
+    freezeTableName: true
+  }
+)
+
+Order.belongsToMany(Product, {through: OrderProduct})
+Product.belongsToMany(Order, {through: OrderProduct})
+
+Product.hasMany(Review)
+Review.belongsTo(Product)
+
+User.hasMany(Review)
+Review.belongsTo(User)
+
+User.hasMany(Order)
+Order.belongsTo(User)
+
 module.exports = {
-  User
+  User,
+  Product,
+  Order,
+  Review,
+  ProductCategory,
+  OrderProduct,
+  db
 }

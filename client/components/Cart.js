4b5a96de7a574a93828d1c2ae886a63abf86d70a
pfgallagher <pfgallagher@gmail.com>
2019-03-04 14:57:55 -0600

    Implement local checkout process. Closes #110.

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import CartItem from './CartItem'
+import {checkout} from './../store/cart'
 
 class Cart extends Component {
   render() {
@@ -13,6 +14,13 @@ class Cart extends Component {
           {`Total: $${this.props.cart
             .reduce((acc, cur) => acc + cur.price * cur.quantity, 0)
             .toFixed(2)}`}
+          {/* Rendering the submit order button here until we figure out where it needs to go. */}
+          <button
+            type="button"
+            onClick={() => this.props.checkout(this.props.cart)}
+          >
+            Submit Order
+          </button>
         </div>
       </div>
     ) : (
@@ -25,4 +33,10 @@ const mapStateToProps = (state, ownProps) => ({
   cart: state.cart
 })
 
-export default connect(mapStateToProps)(Cart)
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  checkout: cart => {
+    dispatch(checkout(cart))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)

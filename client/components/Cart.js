f5a2881a82a3eb639f22fb1ed9bc6b68e0d16114
Kris Gui <gui.kris@gmail.com>
2019-03-04 17:23:04 -0600

    basic stripe integration

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import CartItem from './CartItem'
+import Checkout from './Checkout'
 
 class Cart extends Component {
   render() {
     return this.props.cart ? (
       <div>
         {this.props.cart.map(item => <CartItem key={item.id} item={item} />)}
+        <Checkout />
       </div>
     ) : (
       ''
